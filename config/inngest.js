04dcf67c62335e2c0be0a91c65c59baa434994f4
kavya <kavyadsc@gmail.com>
2025-08-16 14:40:13 +0530

    Fix Inngest route and user schema

--- config/inngest.js
+++ config/inngest.js
@@ -1,28 +1,34 @@
 import { Inngest } from "inngest";
+import connectDB from "@/lib/connectDB"; // make sure correct path
+// import User from "@/models/User"; // make sure correct path
+import User from "../../models/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "shop-next" });
 
-//nngest functon to save user data to a adatabase
-export const syncUserCreation = inngest.createFuncton(
+// Inngest function to save user data to database
+export const syncUserCreation = inngest.createFunction(
   {
     id: "sync-user-from-clerk",
   },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, emal_addresses, mage_url } = event.data;
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + " " + last_name,
+      name: `${first_name} ${last_name}`,
       imageUrl: image_url,
     };
-    awaitconnectDB();
+
+    await connectDB();
     await User.create(userData);
   }
 );
 
-//inngest functon to update wuser data n database
+// Inngest function to update user data in database
 export const syncUserUpdation = inngest.createFunction(
   {
     id: "update-user-from-clerk",
@@ -31,18 +37,20 @@ export const syncUserUpdation = inngest.createFunction(
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
+
     const userData = {
       _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + " " + last_name,
+      name: `${first_name} ${last_name}`,
       imageUrl: image_url,
     };
+
     await connectDB();
     await User.findByIdAndUpdate(id, userData);
   }
 );
 
-//ingest function to delete user data from database
+// Inngest function to delete user data from database
 export const syncUserDeletion = inngest.createFunction(
   {
     id: "delete-user-with-clerk",
